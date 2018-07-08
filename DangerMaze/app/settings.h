e2f6158ab25b07d1603e2ee0e33dc8e8a7544785
Igor <fed-igor-1@ya.ru>
2018-07-08 15:39:39 +0300

    Added settings structure

---
+++ DangerMaze/app/settings.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace app {
+
+    struct Settings {
+        int                         _windowWidth        = 1024;
+        int                         _windowHeight       = 768;
+        bool                        _fullscreenMode     = false;
+    };
+
+}
