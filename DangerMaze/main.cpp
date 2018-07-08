f2d61e4e567fc8047164f6f08930f3a4eb9d3e64
Igor <fed-igor-1@ya.ru>
2018-07-08 15:40:48 +0300

    Updated main file

--- DangerMaze/main.cpp
+++ DangerMaze/main.cpp
@@ -1,8 +1,33 @@
-#include "SDL.h"
+#include <string>
+
+#include "app/settings.h"
+#include "app/application.h"
 
 using namespace std;
+using namespace app;
+
+const string WINDOW_TITLE = "Danger Maze";
 
 int SDL_main(int argc, char* argv[]) {
-    
+    Application app;
+    Settings settings;
+
+    bool initSuccess = app.initialize(WINDOW_TITLE, settings);
+    if (!initSuccess) {
+        return -1;
+    }
+
+    app.run();
+
+    while (app.isRunning()) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            app.handleEvent(event);
+        }
+
+        app.simulate();
+        app.render();
+    }
+
     return 0;
 }
