fac76d08d6830b37e75ff044b032ec89b75f3f7b
Igor <fed-igor-1@ya.ru>
2018-07-11 22:59:12 +0300

    Updated project files

--- DangerMaze/main.cpp
+++ DangerMaze/main.cpp
@@ -10,6 +10,14 @@ const string WINDOW_TITLE       = "Danger Maze";
 const string SETTINGS_FILE_PATH = "settings.xml";
 
 int SDL_main(int argc, char* argv[]) {
+    /*
+        TODO:
+        1. размер экрана по умолчанию 1024x768
+        2. возможнось запуска без библиотек VS
+        3. иконка
+        4. README.md
+    */
+
     Settings settings = Settings::initializeFromFile(SETTINGS_FILE_PATH);
     Application app;
 
