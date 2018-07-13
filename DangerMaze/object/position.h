a7d2b69844e70073021ca81b499d1eaf6455653f
Igor <fed-igor-1@ya.ru>
2018-07-14 00:13:44 +0300

    Added position structure instrean pair<int, int>

---
+++ DangerMaze/object/position.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace object {
+
+    struct Position {
+        int                         row                 = -1;
+        int                         column              = -1;
+    };
+
+}
