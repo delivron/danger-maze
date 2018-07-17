34eb65e57858e758ad3bcb6fbfddd173eb3e3b01
Igor <fed-igor-1@ya.ru>
2018-07-17 10:02:22 +0300

    Removed world settings

--- DangerMaze/app/settings.cpp
+++ DangerMaze/app/settings.cpp
@@ -15,40 +15,15 @@ Settings Settings::initializeFromFile(const std::string& path) {
 
     auto loadSuccess = doc.load_file(path.c_str());
     if (loadSuccess) {
-        DisplaySettings& disp = settings.display;
-        WorldSettings& world = settings.world;
-
         xml_node settingsNode = doc.child("Settings");
 
         xml_node displayNode = settingsNode.child("Display");
         xml_node displaySizeNode = displayNode.child("Size");
         xml_node fullscreenNode = displayNode.child("Fullscreen");
-        tryReadXmlAttribute(disp.width, displaySizeNode.attribute("width"));
-        tryReadXmlAttribute(disp.height, displaySizeNode.attribute("height"));
-        tryReadXmlAttribute(disp.fullscreen, fullscreenNode.attribute("value"));
 
-        xml_node worldNode = settingsNode.child("World");
-        xml_node worldSizeNode = worldNode.child("Size");
-        xml_node enemiesNode = worldNode.child("Enemies");
-        xml_node cannonsNode = worldNode.child("Cannons");
-        tryReadXmlAttribute(world.width, worldSizeNode.attribute("width"));
-        tryReadXmlAttribute(world.height, worldSizeNode.attribute("height"));
-        tryReadXmlAttribute(world.enemiesCount, enemiesNode.attribute("count"));
-        tryReadXmlAttribute(world.cannonsCount, cannonsNode.attribute("count"));
-
-        if (world.width < MIN_WORLD_SIZE) {
-            world.width = MIN_WORLD_SIZE;
-        }
-        else if (world.width > MAX_WORLD_SIZE) {
-            world.width = MIN_WORLD_SIZE;
-        }
-
-        if (world.height < MIN_WORLD_SIZE) {
-            world.height = MIN_WORLD_SIZE;
-        }
-        else if (world.height > MAX_WORLD_SIZE) {
-            world.height = MAX_WORLD_SIZE;
-        }
+        tryReadXmlAttribute(settings.displayWidth, displaySizeNode.attribute("width"));
+        tryReadXmlAttribute(settings.displayHeight, displaySizeNode.attribute("height"));
+        tryReadXmlAttribute(settings.fullscreen, fullscreenNode.attribute("value"));
     }
 
     return settings;
--- DangerMaze/app/settings.h
+++ DangerMaze/app/settings.h
@@ -4,25 +4,10 @@
 
 namespace app {
 
-    const int                       MIN_WORLD_SIZE      = 10;
-    const int                       MAX_WORLD_SIZE      = 100;
-
-    struct DisplaySettings {
-        int                         width               = 1024;
-        int                         height              = 768;
-        bool                        fullscreen          = false;
-    };
-
-    struct WorldSettings {
-        int                         width               = 25;
-        int                         height              = 25;
-        int                         enemiesCount        = 5;
-        int                         cannonsCount        = 5;
-    };
-
     struct Settings {
-        DisplaySettings             display;
-        WorldSettings               world;
+        int                         displayWidth        = 1024;
+        int                         displayHeight       = 768;
+        bool                        fullscreen          = false;
 
         static Settings             initializeFromFile(const std::string& path);
     };
